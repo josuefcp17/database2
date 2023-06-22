340736c9f57c83633f7a98d691447b21ffefb89f
jos <85748945+josuefcp17@users.noreply.github.com>
2023-06-22 16:25:00 -0500

    Update consultas.py

--- consultas.py
+++ consultas.py
@@ -1,15 +1,27 @@
 from pymongo import MongoClient
-from py2neo import Graph,Node,Relationship
+from py2neo import Graph, Node, Relationship
 
+# Conexión a MongoDB
 client = MongoClient("mongodb://localhost:27017/")
-db= client["basesito"]
-collection= db["basesito"]
-#GraphDataBase
-neo4j_driver= Graph.driver("bolt://localhost:7687/", 
+db = client["libreria"]
+collection = db["libreria"]
+
+query = {"sales":{"$gte":100}}
+results = collection.find(query)
+
+# Conexión a Neo4j
+graph = Graph("bolt://localhost:7687", 
 auth=("neo4j", "12345678"))
-neo4j_session = neo4j_driver.session()
 
-query={"score":{"$gt":5}}
-results= collection.find(query)
+for result in results:
 
-#for query in employees_col.find():
+    # Crear el nodo 
+    obra_node = Node("Obra", Author=result["book"])
+    graph.create(obra_node)
+    #crear el nodo si no existe
+    genre_node = Node("genre", genre=result["genre"])
+    graph.merge(genre_node, "genre", "genre")
+
+    # Crear la relación 
+    relationship = Relationship(obra_node, "Genero", genre_node)
+    graph.create(relationship)
